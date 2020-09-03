de2d32d2ce0eab03a0ad2c74609e837eb416bc45
Taras-SH13 <13tsh2019@gmail.com>
2020-09-03 12:59:22 +0300

    Add first handler in chain.

--- src/main/java/com/skillsup/patterns/UserAuthenticatorImpl.java
+++ src/main/java/com/skillsup/patterns/UserAuthenticatorImpl.java
@@ -1,14 +1,30 @@
 package com.skillsup.patterns;
 
+import com.skillsup.patterns.db.EntityRepository;
+import com.skillsup.patterns.db.EntityRepositoryImpl;
+import com.skillsup.patterns.dto.Converter;
 import com.skillsup.patterns.dto.Credentials;
+import com.skillsup.patterns.dto.User;
 
-public class UserAuthenticatorImpl implements UserAuthenticator{
+public class UserAuthenticatorImpl implements UserAuthenticator {
+    UserAuthenticator next;
+    EntityRepositoryImpl entityRepository = new EntityRepositoryImpl();
+    User user;
+
+    public void setNext(UserAuthenticator next) {
+        this.next = next;
+    }
 
     @Override
     public UserRole authenticate(Credentials credentials) {
 
-
-
-        return null;
-    }
+      user=Converter.userEntitytoUser(entityRepository.findUser(Converter.credentialsToCredentialsEntity(credentials)));
+      if(user==null){
+          System.out.println("This user is not registered!");
+      }
+      if(next!=null){
+         next.authenticate(credentials);
+      }
+      return UserRole.UNKNOWN;
+}
 }
